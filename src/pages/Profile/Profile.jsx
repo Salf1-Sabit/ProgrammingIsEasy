da1bffbb78807ec953e6bd56573219b555b08942
Salfi Sabit <salfisabit@gmail.com>
2024-05-19 00:04:41 +0600

    Complete Profile-Page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-import ProfileSkeleton from "./ProfileSkeleton";
+import React, { useState } from "react";
 import RatingIcon from "../../assets/icons/ranking.png";
 import ContributionIcon from "../../assets/icons/contribution.png";
 import FriendsIcon from "../../assets/icons/contacts.png";
@@ -20,15 +19,15 @@ const RANK_COLOR = {
   "legendary grandmaster": "#FF1010",
 };
 
-const Profile = ({}) => {
+const Profile = () => {
   const [user, setUser] = useState(null);
   const [totalContest, setTotalContest] = useState(null);
+  const [userName, setUserName] = useState("");
 
-  useEffect(() => {
-    const USER_NAME = "Salfi_Sabit";
+  const getUser = () => {
     axios
       .get(
-        `https://codeforces.com/api/user.info?handles=${USER_NAME}&checkHistoricHandles=false`
+        `https://codeforces.com/api/user.info?handles=${userName}&checkHistoricHandles=false`
       )
       .then((response) => {
         setUser(response.data.result[0]);
@@ -38,105 +37,141 @@ const Profile = ({}) => {
       });
 
     axios
-      .get(`https://codeforces.com/api/user.rating?handle=${USER_NAME}`)
+      .get(`https://codeforces.com/api/user.rating?handle=${userName}`)
       .then((response) => {
         setTotalContest(response.data.result.length);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, []);
+  };
 
   return (
     <>
-      {user ? (
-        <div className="profile">
-          <img
-            className="profile__img"
-            src={user.titlePhoto}
-            alt="User avatar"
-          />
+      <div className="profile-container">
+        {user ? (
+          <div className="profile-outer-container">
+            <div className="profile">
+              <img
+                className="profile__img"
+                src={user.titlePhoto}
+                alt="User avatar"
+              />
 
-          <div className="profile__description">
-            <h3 className="profile__description__current-rank">
-              <span style={{ color: `${RANK_COLOR[user.rank]}` }}>
-                {user.rank}
-              </span>
-            </h3>
-            <h1
-              className="profile__description__user-name"
-              style={{ color: `${RANK_COLOR[user.rank]}` }}
-            >
-              {user.handle}
-            </h1>
-            <p className="profile__description__city">
-              <span style={{ color: "#0F0FCF", textDecoration: "underline" }}>
-                {user.city}
-              </span>
-              ,{" "}
-              <span style={{ color: "#0F0FCF", textDecoration: "underline" }}>
-                {user.country}
-              </span>
-            </p>
+              <div className="profile__description">
+                <h3 className="profile__description__current-rank">
+                  <span style={{ color: `${RANK_COLOR[user.rank]}` }}>
+                    {user.rank}
+                  </span>
+                </h3>
+                <h1
+                  className="profile__description__user-name"
+                  style={{ color: `${RANK_COLOR[user.rank]}` }}
+                >
+                  {user.handle}
+                </h1>
+                <p className="profile__description__city">
+                  <span
+                    style={{ color: "#0F0FCF", textDecoration: "underline" }}
+                  >
+                    {user.city}
+                  </span>
+                  ,{" "}
+                  <span
+                    style={{ color: "#0F0FCF", textDecoration: "underline" }}
+                  >
+                    {user.country}
+                  </span>
+                </p>
 
-            <p className="profile__description__organization">
-              From{" "}
-              <span style={{ color: "#0F0FCF", textDecoration: "underline" }}>
-                {user.organization}
-              </span>
-            </p>
+                <p className="profile__description__organization">
+                  From{" "}
+                  <span
+                    style={{ color: "#0F0FCF", textDecoration: "underline" }}
+                  >
+                    {user.organization}
+                  </span>
+                </p>
 
-            <p className="profile__description__details">
-              <img
-                src={RatingIcon}
-                alt="Rating icon"
-                style={{ width: "1.5em" }}
-              />
-              <span>
-                Contest rating:{" "}
-                <span style={{ color: `${RANK_COLOR[user.rank]}` }}>
-                  {user.rating}
-                </span>{" "}
-                (max.{" "}
-                <span style={{ color: `${RANK_COLOR[user.maxRank]}` }}>
-                  {user.maxRank}
-                </span>
-                ,{" "}
-                <span style={{ color: `${RANK_COLOR[user.maxRank]}` }}>
-                  {user.maxRating}
-                </span>
-                )
-              </span>
-            </p>
-            <p className="profile__description__details">
-              <img
-                src={ContributionIcon}
-                alt="Rating icon"
-                style={{ width: "1.5em" }}
-              />
-              <span>Contribution: {user.contribution}</span>
-            </p>
-            <p className="profile__description__details">
-              <img
-                src={FriendsIcon}
-                alt="Rating icon"
-                style={{ width: "1.5em" }}
-              />
-              <span>Friends of: {user.friendOfCount}</span>
-            </p>
-            <p className="profile__description__details">
-              <img
-                src={ContestIcon}
-                alt="Rating icon"
-                style={{ width: "1.5em" }}
-              />
-              <span>Contest participated: {totalContest}</span>
-            </p>
+                <p className="profile__description__details">
+                  <img
+                    src={RatingIcon}
+                    alt="Rating icon"
+                    style={{ width: "1.5em" }}
+                  />
+                  <span>
+                    Contest rating:{" "}
+                    <span style={{ color: `${RANK_COLOR[user.rank]}` }}>
+                      {user.rating}
+                    </span>{" "}
+                    (max.{" "}
+                    <span style={{ color: `${RANK_COLOR[user.maxRank]}` }}>
+                      {user.maxRank}
+                    </span>
+                    ,{" "}
+                    <span style={{ color: `${RANK_COLOR[user.maxRank]}` }}>
+                      {user.maxRating}
+                    </span>
+                    )
+                  </span>
+                </p>
+                <p className="profile__description__details">
+                  <img
+                    src={ContributionIcon}
+                    alt="Rating icon"
+                    style={{ width: "1.5em" }}
+                  />
+                  <span>Contribution: {user.contribution}</span>
+                </p>
+                <p className="profile__description__details">
+                  <img
+                    src={FriendsIcon}
+                    alt="Rating icon"
+                    style={{ width: "1.5em" }}
+                  />
+                  <span>Friends of: {user.friendOfCount}</span>
+                </p>
+                <p className="profile__description__details">
+                  <img
+                    src={ContestIcon}
+                    alt="Rating icon"
+                    style={{ width: "1.5em" }}
+                  />
+                  <span>Contest participated: {totalContest}</span>
+                </p>
+              </div>
+            </div>
+            <div className="profile__btn-container">
+              <button
+                className="profile__btn-handle-remove"
+                onClick={() => {
+                  setUserName("");
+                  setUser("");
+                }}
+              >
+                Remove Profile
+              </button>
+            </div>
           </div>
-        </div>
-      ) : (
-        <ProfileSkeleton />
-      )}
+        ) : (
+          <div className="profile__default-page">
+            <h2 className="profile__title">
+              Please enter your Codeforces handle
+            </h2>
+            <div className="profile__input-container">
+              <input
+                className="profile__handle-input"
+                type="text"
+                placeholder="example: tourist"
+                onChange={(event) => setUserName(event.target.value)}
+              />
+              <button className="add-profile-button" onClick={getUser}>
+                Add Profile
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 };
