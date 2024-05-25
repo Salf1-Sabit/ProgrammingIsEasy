6bb6a17500ad6cfa70641eec2a6c23ac66cbea61
Salfi Sabit <salfisabit@gmail.com>
2024-05-25 20:51:32 +0600

    modify home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "../assets/styles/Home.css";
+import { RiBracesLine } from "@remixicon/react";
 
 const Home = () => {
-  return <div>Welcome to Home</div>;
+  return (
+    <div className="home-container">
+      <div className="blurred-circle"></div>
+      <div className="blurred-circle"></div>
+      <div className="hero-container">
+        <div className="hero__title">
+          <h1 className="home-heading">
+            Your Journey to <strong>Coding</strong>
+          </h1>
+          <h1 className="home-heading">
+            <strong>Mastery</strong> Begins Here
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
